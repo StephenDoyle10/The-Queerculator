454fcc43af80adbc10c53bd4944299239ac71848
StephenDoyle10 <62725427+StephenDoyle10@users.noreply.github.com>
2020-10-31 08:25:21 +0000

    Update script.js
    Added changes so that if user presses two arithmetic symbols in a row, the first one is deleted.
    
    Also, added to the function so that division symbol is displayed as '÷' and not as '/', and so that multiplication symbol is displayed as 'x' and not as '*'

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 
 
+//Code so that the '*' character is displayed as 'x' on users browser, and so that '/' character is displayed as '÷'.
+//Also includes code so if user pushes two arithmetic buttons in a row, the first one deleted and only the most recently pressed button displays.
+const calC =(c)=> {
+let str=form.panel.value+c;
+let finalChar=str.charAt(str.length-1)
+let newStr = str.replace(/[x÷*/+-][x÷*/+-]/, finalChar).replace('*', 'x').replace('/',"÷")
 
-function calC(c) {
+	form.panel.value=newStr
+    }
 
-    form.panel.value = form.panel.value + c;
-
-
+//This converts 'x' and '÷' back to '/' and '*' so that 'eval()' function will work
+const workOut=(sum)=>{
+	let str =form.panel.value;
+	let newStr = str.replace(/x/g, '*').replace(/÷/g,'/')
+	console.log(newStr)
+	form.panel.value=eval(newStr)
 }
 
 
-function CE() {
+const CE=()=> {
 
     
     form.panel.value = "";
